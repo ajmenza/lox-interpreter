f25bf92b2638d95399aa48d093e483b152437eb3
ajmenza <ajm043@bucknell.edu>
2022-11-21 20:33:55 -0500

    finished scanner

--- src/main/java/lox/Scanner.java
+++ src/main/java/lox/Scanner.java
@@ -9,6 +9,28 @@ import static lox.TokenType.*;
 
 
 public class Scanner {
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and", AND);
+        keywords.put("class", CLASS);
+        keywords.put("else", ELSE);
+        keywords.put("false", FALSE);
+        keywords.put("for", FOR);
+        keywords.put("fun", FUN);
+        keywords.put("if", IF);
+        keywords.put("nil", NIL);
+        keywords.put("or", OR);
+        keywords.put("print", PRINT);
+        keywords.put("return", RETURN);
+        keywords.put("super", SUPER);
+        keywords.put("this", THIS);
+        keywords.put("true", TRUE);
+        keywords.put("var", VAR);
+        keywords.put("while", WHILE);
+    }
+
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
     private int start = 0;
@@ -30,6 +52,7 @@ public class Scanner {
         return tokens;
     }
 
+    // Checks to see if we are at the end of the file
     private boolean isAtEnd() {
         return current >= source.length();
     }
@@ -80,8 +103,10 @@ public class Scanner {
                 addToken(match('=') ? GREATER_EQUAL : GREATER);
                 break;
             case '/':
+                // If char immediately after is '/' keep advancing until we reach EOL (this is a comment)
                 if (match('/')) {
                     while (peek() != '\n' && !isAtEnd()) advance();
+                    // Otherwise, this is a divide symbol
                 } else {
                     addToken(SLASH);
                 }
@@ -94,10 +119,78 @@ public class Scanner {
             case '\n':
                 line++;
                 break;
+            case '"':
+                string();
+                break;
             default:
-                Lox.error(line, "Unexpected character.");
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
                 break;
         }
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    private void identifier() {
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if (type == null) type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        // Look for fractional part
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+
+            while (isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    // Return character after next in source file
+    private char peekNext() {
+        // If end of file return null char
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
+    }
+
+    private void string() {
+        // Keep advancing until we reach end quote or end of file
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+        }
+
+        // The closing quote
+        advance();
+
+        // Trim the surrounding quotes
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
     }
 
     private char advance() {
@@ -121,9 +214,11 @@ public class Scanner {
         return true;
     }
 
+    // Return next char in source file
     private char peek() {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
     }
+
 
 }
